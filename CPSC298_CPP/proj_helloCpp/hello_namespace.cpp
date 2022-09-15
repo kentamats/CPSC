244d093e942a7e4410dbf3dbe4b8969381e641ee
kentamats <matsuo@chapman.edu>
2022-09-15 04:02:49 +0000

    Proj_HelloCpp hello with namespace

---
+++ CPSC298_CPP/proj_helloCpp/hello_namespace.cpp
@@ -0,0 +1,10 @@
+/// @file hello_namespace.cpp
+/// @brief Outputs Hello C++! and uses a namespace.
+/// @author Kentaro Matsuo
+#include <iostream>
+using namespace std;
+int main()
+{
+ cout << "Hello C++!" << endl;
+ return 0;
+}
